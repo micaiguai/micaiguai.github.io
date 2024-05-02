71990adb61c2e938f42fe23faac1ae7ea0466c41
micaiguai <1399880823@qq.com>
2024-05-02 22:50:42 +0800

    feat: sandbox_canvas update

---
+++ sandbox_canvas/assets/user-_CgFLsgf.js
@@ -0,0 +1,1 @@
+import{A as m,k as r,B as t}from"./app-Btv13WWp.js";const l=m("user",()=>{const e=r(""),s=r(new Set),o=t(()=>Array.from(s.value)),u=t(()=>o.value.filter(a=>a!==e.value));function n(a){e.value&&s.value.add(e.value),e.value=a}return{setNewName:n,otherNames:u,savedName:e}});export{l as u};
